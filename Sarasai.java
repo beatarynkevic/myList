16f8ca4f84cace8779620d4909e43268d2189add
Beata <62422202+beatarynkevic@users.noreply.github.com>
2021-01-24 19:08:43 +0200

    Update Sarasai.java

--- Sarasai.java
+++ Sarasai.java
@@ -5,15 +5,17 @@ public class Sarasai {
     public static void main(String[] args) {
         
         MyList ml = new MyList();
-        ml.printAll();
         System.out.println(ml.size());
         ml.add("Labas");
+        
         ml.printAll();
         System.out.println(ml.size());
         ml.add("Pasauli");
+        
         ml.printAll();
         System.out.println(ml.size());
         ml.add("!");
+        
         System.out.println(ml.size()); //3
         System.out.println(ml); //[Labas, Pasauli !]
         System.out.println(ml.get(1)); //Pasauli
