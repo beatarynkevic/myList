4b7e0998b5fc7e3834eead16e83f657df7eb52a5
Beata <62422202+beatarynkevic@users.noreply.github.com>
2021-01-24 18:44:44 +0200

    Create MyList.java

---
+++ MyList.java
@@ -0,0 +1,52 @@
+package sarasai;
+
+public class MyList {
+    
+    private Object[] list;
+    
+    public MyList() {
+        this.list = new Object[0];
+    }
+    public void add(Object o) {
+        Object[] newList = new Object[this.list.length + 1];
+        for (int i = 0; i < list.length; i++) {
+            newList[i] = list[i];
+        }
+        newList[newList.length - 1] = o;
+        this.list = newList;
+    }
+    
+    public Object get(int index) {
+        if (index < 0 || index >= this.list.length) {
+            return null;
+        }
+        return this.list[index];
+    }
+    
+    public void set(int index, Object o) {
+        
+    }
+    
+    public void remove(int index) {
+        
+    }
+
+//    public void insert(int index, Object o) {     kas gudresnis???
+//        
+//    }
+    public int size() {
+        return this.list.length;
+    }
+
+    @Override
+    public String toString() { //turi atspausdinti objektu sarasa
+        return "[" + "]";
+    }
+    
+    public void printAll() {
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+        }
+        System.out.println("--------------");
+    }
+}
